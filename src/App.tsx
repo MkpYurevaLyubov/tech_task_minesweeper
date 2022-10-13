de01fa337aa980cf9ad42dda964a5aeeeb78323f
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-10-13 11:05:08 +0300

    connect to ws

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const socket = useRef<any>();
+  const [connected, setConnected] = useState<boolean>(false);
+  const [level, setLevel] = useState<number>(1);
+
+  useEffect((): void => {
+    socket.current = new WebSocket('wss://hometask.eg1236.com/game1/');
+
+    socket.current.onopen= (): void => {
+      try {
+        setConnected(true);
+        sentMessage();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    socket.current.onmessage = (event: any): void => {
+      const message = event.data;
+
+      if (message === 'new: OK') {
+        sentMessage('map');
+      }
+    }
+
+    socket.current.onclose = (): void => {
+      console.log('Socken закрыт');
+    }
+
+    socket.current.onerror = (): void => {
+      console.log('Socken произошла ошибка');
+    }
+  }, []);
+
+  const sentMessage = (message: string = 'new 1') => {
+    socket.current.send(message);
+  };
+
+  if (!connected) {
+    return <h1>Соединение не установлено</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
